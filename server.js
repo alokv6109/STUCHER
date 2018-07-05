38a38bbaa2d253dacbdbff1e997196376ecd4ffc
sj_kumar <sonuyadav0521@gmail.com>
2018-07-05 12:54:39 +0530

    changes

--- server.js
+++ server.js
@@ -1,130 +1,147 @@
 var express = require('express');
-var path=require('path')
+var path = require('path')
 var app = express();
 var md5 = require('md5');
 var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({extended:true})
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
 app.use(urlencodedParser);
 var mysql = require('mysql');
 var con = mysql.createConnection
-({
-  host:"localhost",
-  user:"root",
+  ({
+    host: "localhost",
+    user: "root",
 
-  password:"",
+    password: "",
 
 
-  database:"project_database"
-})
-con.connect(function(err)
-{
-  if(err) throw err;
+    database: "project_database"
+  })
+con.connect(function (err) {
+  if (err) throw err;
   console.log("connected! yo db");
 });
-app.get('/', function(req , res)
-{
+
+// var htmlPath = path.join("F:/intern-project/frontend/" + 'assets');
+app.use(express.static('../frontend/assets'));
+
+app.get('/', function (req, res) {
   console.log("your index page has loaded successfully");
-  res.sendFile("/intern-project/frontend/" +  "index.html");
+  res.sendFile(path.resolve('../frontend/index.html'));
 })
-app.get('/teacherlogin', function(req , res)
-{
+app.get('/teacherlogin', function (req, res) {
   console.log("your teacher page has loaded successfully");
-  res.sendFile("/intern-project/frontend/" +  "teacher_login.html");
+  res.sendFile(path.resolve('../frontend/assets/html/teacher_login.html'));
 })
-app.get('/studentlogin', function(req , res)
-{
+app.get('/studentlogin', function (req, res) {
   console.log("your student page has loaded successfully");
-  res.sendFile( "/intern-project/frontend/" +  "login.html");
+  res.sendFile(path.resolve('../frontend/assets/html/student_login.html'));
 })
-app.get('/newstudent', function(req , res)
-{
+app.get('/newstudent', function (req, res) {
   console.log("your signup page for student has loaded successfully");
-  res.sendFile( "/intern-project/frontend/" +  "signup.html");
+  res.sendFile(path.resolve('../frontend/assets/html/student_signup.html'));
 })
-app.get('/newteacher', function(req , res)
-{
+app.get('/newteacher', function (req, res) {
   console.log("your signup page for teacher has loaded successfully");
-  res.sendFile("/intern-project/frontend/" +  "teacher_signup.html");
+  res.sendFile(path.resolve('../frontend/assets/html/teacher_signup.html'));
 })
 
-app.post('/process_stud' , function(req, res)
-{
+app.post('/process_stud', function (req, res) {
 
   var sql = "select * from users where roll_no = ? or email_id=?";
-  con.query(sql, [req.body.login_id, req.body.login_id], function(err , result)
-  {
-    if(err) throw err;
-    if(result.length<=0)
-    {
+  con.query(sql, [req.body.login_id, req.body.login_id], function (err, result) {
+    if (err) throw err;
+    if (result.length <= 0) {
       res.end("Please! check your username once");
     }
-    else
-    {
-      if((result[0].password)==(md5(req.body.password)))
-      {
+    else {
+      if ((result[0].password) == (md5(req.body.password))) {
         var date = new Date();
         console.log(date);
-        var token1=jwt.sign({date}, 'i_am_alok',function(err, token)//token assigned in this line
+        var token1 = jwt.sign({ date }, 'i_am_alok', function (err, token)//token assigned in this line
         {
           // res.json(
           //   {
           //     token
           //   });
-             console.log(token);
-             con.query("update users set  login_token=?  where email_id=?", [token,result[0].email_id],function( err, result)
-           {
-               if(err) throw err;
-                //console.log(token1 +"this oi tsdfdv");
-             })
-
+          console.log(token);
+          con.query("update users set  login_token=?  where email_id=?", [token, result[0].email_id], function (err, result) {
+            if (err) throw err;
+            //console.log(token1 +"this oi tsdfdv");
+            // app.get('/process_stud',function(err,res){
+            //   if(err) throw err
+            //   console.log(result[0])
+            // });
+            // app.get('/process_stud', function (req, res) {
+            //   var q = "select * from users where roll_no=?";
+            //   con.query(q, [req.body.login_id], function (err, result) {
+            //     if (err) throw err
+            //     console.log("Hello ")
+            //     console.log(req)
+            //     // console.log(result[0])
+            //     // res.send(result[0])
+            //   })
+            // });
           });
-          //console.log(token1 + "   fsdg");
 
-        res.sendFile("/intern-project/frontend/" + "student_details.html" );
-        } else {
-          res.end("WRONG CREDENTIALS");
-        }
-      }
-    })
-    console.log("your student login page is processing some request");
-  })
+        });
+        //console.log(token1 + "   fsdg");
 
-  app.post('/process_teach' , function(req, res){
-    //  console.log(md5('alok'));
-    //console.log(req);
-    var sql = "select * from users where roll_no = ? or email_id=?";
-    con.query(sql, [req.body.emp_id, req.body.emp_id], function(err , result)
-    {
-      if(err) throw err;
-
-      if(result.length<=0)
-      {
-        res.end("Please! check your username once");
+        res.render()
       }
       else {
-        if((result[0].password)==(md5(req.body.password)))
+        res.end("WRONG CREDENTIALS");
+      }
+    }
+  })
+  console.log("your student login page is processing some request");
+})
 
+app.get('/process_stud',function(req,res){
+  var q="select * from users where roll_no=?";
+  console.log('sanjay',req.body);
+  
+  
+  con.query(q,[req.body.login_id],function(err,result){
+    if(err) throw err
+    console.log("Hello ")
+
+    console.log(result[0])
+    res.send(result[0])
+  })
+});
+
+
+app.post('/process_teach', function (req, res) {
+  //  console.log(md5('alok'));
+  //console.log(req);
+  var sql = "select * from users where roll_no = ? or email_id=?";
+  con.query(sql, [req.body.emp_id, req.body.emp_id], function (err, result) {
+    if (err) throw err;
+
+    if (result.length <= 0) {
+      res.end("Please! check your username once");
+    }
+    else {
+      if ((result[0].password) == (md5(req.body.password))) {
+        var date = new Date();
+        console.log(date);
+        var token1 = jwt.sign({ date }, 'i_am_alok', function (err, token)//token assigned in this line
         {
-          var date = new Date();
-          console.log(date);
-          var token1=jwt.sign({date}, 'i_am_alok',function(err, token)//token assigned in this line
-          {
-            // res.json(
-            //   {
-            //     token
-            //   });
-               console.log(token);
-               con.query("update users set  login_token=?  where email_id=?", [token,result[0].email_id],function( err, result)
-             {
-                 if(err) throw err;
-                  //console.log(token1 +"this oi tsdfdv");
-               })
+          // res.json(
+          //   {
+          //     token
+          //   });
+          console.log(token);
+          con.query("update users set  login_token=?  where email_id=?", [token, result[0].email_id], function (err, result) {
+            if (err) throw err;
+            //console.log(token1 +"this oi tsdfdv");
+          })
 
-            });
+        });
 
 
-            res.sendFile( "/intern-project/frontend/" +"teacher_details.html" );
+        res.sendFile("/intern-project/frontend/assets/html/" + "teacher_details.html");
       }
 
 
@@ -135,49 +152,44 @@ app.post('/process_stud' , function(req, res)
   })
   console.log("your teacher login page is processing some request");
 })
-app.post('/register_stud', function(req,res)
-{
+app.post('/register_stud', function (req, res) {
   var sql = "select * from users where roll_no = ? or mobile_number=? ";
-  con.query(sql , [req.body.login_id, req.body.login_id], function(err, result){
-    if(err) throw err;
-    if(result.length>0){
+  con.query(sql, [req.body.login_id, req.body.login_id], function (err, result) {
+    if (err) throw err;
+    if (result.length > 0) {
       res.end("roll_no or mobile_no already exists! please check them once");
-    }else{
+    } else {
       var data = new Date(); // without jquery remove this $.now()
       console.log(data)// Thu Jun 23 2016 15:48:24 GMT+0530 (IST)
 
-      var sql= "insert into users(first_name, last_name, dob, roll_no, branch_id,email_id, mobile_number, password, created_at, modified_at, image) values(?,?,?,?,?,?,?,?,?,?,?)";
-      con.query(sql, [req.body.first_name, req.body.last_name, req.body.dob, req.body.roll_no, req.body.branch, req.body.email, req.body.mobile, md5(req.body.password),data,data, req.body.pic], function(err, result){
-        if(err) throw err;
+      var sql = "insert into users(first_name, last_name, dob, roll_no, branch_id,email_id, mobile_number, password, created_at, modified_at, image) values(?,?,?,?,?,?,?,?,?,?,?)";
+      con.query(sql, [req.body.first_name, req.body.last_name, req.body.dob, req.body.roll_no, req.body.branch, req.body.email, req.body.mobile, md5(req.body.password), data, data, req.body.pic], function (err, result) {
+        if (err) throw err;
         console.log("user added to db with id " + result.insertId);
       })
-      res.sendFile("/intern-project/frontend/" + "student_details.html" );
+      res.sendFile(path.resolve('../frontend/assets/html/student_details.html'));
     }
   })
 })
-app.post('/register_teach', function(req,res)
-{
+app.post('/register_teach', function (req, res) {
   var sql = "select * from users where roll_no = ? or mobile_number=? ";
-  con.query(sql , [req.body.emp_id, req.body.mobile], function(err, result){
-    if(err) throw err;
-    if(result.length>0){
+  con.query(sql, [req.body.emp_id, req.body.mobile], function (err, result) {
+    if (err) throw err;
+    if (result.length > 0) {
       res.end("emp_id or mobile_no already exists! please check them once");
-    }else{
+    } else {
       var data = new Date(); // without jquery remove this $.now()
       console.log(data)// Thu Jun 23 2016 15:48:24 GMT+0530 (IST)
 
-      var sql= "insert into users(first_name, last_name, dob, roll_no, branch_id,email_id, mobile_number, password, created_at,modified_at, image) values(?,?,?,?,?,?,?,?,?,?,?)";
-      con.query(sql, [req.body.first_name, req.body.last_name, req.body.dob, req.body.emp_id, req.body.department, req.body.email, req.body.mobile, md5(req.body.password),data,data, req.body.pic], function(err, result){
-        if(err) throw err;
+      var sql = "insert into users(first_name, last_name, dob, roll_no, branch_id,email_id, mobile_number, password, created_at,modified_at, image) values(?,?,?,?,?,?,?,?,?,?,?)";
+      con.query(sql, [req.body.first_name, req.body.last_name, req.body.dob, req.body.emp_id, req.body.department, req.body.email, req.body.mobile, md5(req.body.password), data, data, req.body.pic], function (err, result) {
+        if (err) throw err;
         console.log("user added to db with id " + result.insertId);
       })
-      res.sendFile("/intern-project/frontend/" + "teacher_details.html" );
+      res.sendFile(path.resolve('../frontend/assets/html/teacher_details.html'));
     }
   })
 })
-var htmlPath = path.join("F:/intern-project/"+'frontend');
-app.use(express.static(htmlPath));
-
 
 var server = app.listen(8081, function () {
   var host = server.address().address
