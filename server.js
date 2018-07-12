3bc5afd653930f32d1878f15218b2398ddc52c3d
snj_kumar <sonuyadav0521@gmail.com>
2018-07-12 13:34:20 +0530

    small change

--- server.js
+++ server.js
@@ -31,6 +31,42 @@ con.connect(function (err) {
 	console.log("connected! yo db");
 });
 
+// ******************      MULTER CONFIGURATION     *************
+const multerConfig = {
+    
+	storage: multer.diskStorage({
+	 //Setup where the user's file will go
+	 destination: function(req, file, next){
+	   next(null, './images');
+	   },   
+		
+		//Then give the file a unique name
+		filename: function(req, file, next){
+			console.log(file);
+			const ext = file.mimetype.split('/')[1];
+			next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+		  }
+		}),   
+		
+		//A means of ensuring only images are uploaded. 
+		fileFilter: function(req, file, next){
+			  if(!file){
+				next();
+			  }
+			const image = file.mimetype.startsWith('image/');
+			if(image){
+			  console.log('photo uploaded');
+			  next(null, true);
+			}else{
+			  console.log("file not supported");
+			  
+			  //TODO:  A better message response to user on failure.
+			  return next();
+			}
+		}
+	  };
+
+
 app.use(express.static('../frontend/assets'));
 
 app.get('/', function (req, res) {
@@ -198,41 +234,6 @@ app.post('/register_stud',multer(multerConfig).single('pc'), function (req, res)
 		}
 	})
 })
-
-// ******************      MULTER CONFIGURATION     *************
-const multerConfig = {
-    
-	storage: multer.diskStorage({
-	 //Setup where the user's file will go
-	 destination: function(req, file, next){
-	   next(null, './images');
-	   },   
-		
-		//Then give the file a unique name
-		filename: function(req, file, next){
-			console.log(file);
-			const ext = file.mimetype.split('/')[1];
-			next(null, file.fieldname + '-' + Date.now() + '.'+ext);
-		  }
-		}),   
-		
-		//A means of ensuring only images are uploaded. 
-		fileFilter: function(req, file, next){
-			  if(!file){
-				next();
-			  }
-			const image = file.mimetype.startsWith('image/');
-			if(image){
-			  console.log('photo uploaded');
-			  next(null, true);
-			}else{
-			  console.log("file not supported");
-			  
-			  //TODO:  A better message response to user on failure.
-			  return next();
-			}
-		}
-	  };
 
 app.post('/register_teach',multer(multerConfig).single('pc'), function (req, res) {
 	// console.log("FILE NAME ",req.file.originalname)
