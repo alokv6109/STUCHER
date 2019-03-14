aba5c9f91b91accb05122b63367abc1e6afbe31f
Alok_6109 <alokv6109@gmail.com>
2019-03-14 16:11:36 +0530

    extra xhanges

--- server.js
+++ server.js
@@ -498,5 +498,5 @@ var server = app.listen(port, function () {
 	//var host = server.address().address
 	//var port = server.address().port
 	//console.log("Example app listening at http://%s:%s", host, port)
-	console.log(app running)
+	console.log("app running")
 })
